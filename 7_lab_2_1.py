ff6c4e836e90635a6e021af4300f088f92d60957
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2020-03-18 15:26:05 +0300

    no message

--- 7_lab_2_1.py
+++ 7_lab_2_1.py
@@ -1,2 +1,26 @@
-test=list(range(10))
-print(test)
+import math
+
+# n = int(input("input a number greater than 0: "))
+test = list(range(17))
+wh = len(test)
+# количество блоков
+nu = int(math.sqrt(wh - 1)) + 1
+# количество чисел в блоках.
+le = int((wh - 1) / nu) + 1
+# считаем сумму в блоках
+presum = [0] * nu
+for i in test:
+    presum[int(i / le)] += test[i]
+ll = 2
+rr = 8
+sum = 0
+# тк нумерация идет с нуля, предположим, что пользователь считает, что 0й элемент это первый
+i = ll-1
+while i < rr:
+    if i % le == 0 and i + le - 1 <= rr-1:
+        sum += presum[int(i / le)]
+        i += le
+    else:
+        sum += test[i]
+        i += 1
+print(sum)
