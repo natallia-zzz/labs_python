883c8ce00f4fa630fc6e0d3ce8d3657a5a63a969
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2020-04-08 11:56:26 +0300

    no message

--- n7_lab_2_4.py
+++ n7_lab_2_4.py
@@ -1,3 +1,4 @@
+import argparse
 
 
 def flatten_it(some_list):
@@ -12,15 +13,34 @@ def flatten_it(some_list):
                 a.append(elem)
         count = 0
         for el in a:
-            if isinstance(el, int):
+            if not isinstance(el, list):
                 count += 1
         if count == len(a):
             break
         else:
             b = a
             a = []
+    convert_to_int(a)  # в argparse элементы сохраняются как string. переводим в int
+
+
+def convert_to_int(some_list):
+    a = []
+    for i in some_list:
+        try:
+            i = int(i)
+            a.append(i)
+        except ValueError:
+            pass
     print(a)
 
 
-a_list = [1, [2, 3], 4, [5, [6, 7], 8], 9]
-flatten_it(a_list)
+def main():
+    a_list = [1, [2, 3], 4, [5, [6, 7], 8], 9]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--list", type=list, nargs='*', help="array", default=a_list)
+    args = parser.parse_args()
+    flatten_it(args.list)
+
+
+if __name__ == "__main__":
+    main()
