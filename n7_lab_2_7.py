9d355082ae7faa19c9139d3439e764200febde4e
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2020-04-09 17:39:09 +0300

    no message

--- n7_lab_2_7.py
+++ n7_lab_2_7.py
@@ -2,7 +2,7 @@ import argparse
 
 
 def leo_num(n):
-    if not isinstance(n,int):
+    if not isinstance(n, int):
         print("неправильно введенное число")
     elif n < 0:
         print("нет такого числа. введенный параметр отрицательный ")
@@ -15,9 +15,25 @@ def leo_num(n):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("num", type=int, required=True, help="integer for function", nargs=1)  # только одно число
+    parser.add_argument("-n", "--num", type=int, help="integer for function", nargs=1)  # только одно число
     args = parser.parse_args()
-    leo_num(args.num)
+    if args.num is not None:
+        leo_num(args.num)
+    else:
+        print("Напишите exit когда захотите завершить работу")
+        n = input("введите число: ")
+        while True:
+            try:
+                if n == 'exit':
+                    break
+                n = int(n)
+                if n < 0:
+                    n = input("Число отрицательное. Повторите ввод: ")
+                else:
+                    leo_num(n)
+                    n = input("введите число: ")
+            except ValueError:
+                n = input("Это не число. Повторите ввод: ")
 
 
 if __name__ == '__main__':
