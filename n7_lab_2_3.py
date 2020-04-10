48fc061badf8692e453807b51b4a6dc1968dfba0
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2020-04-10 22:21:19 +0300

    no message

--- n7_lab_2_3.py
+++ n7_lab_2_3.py
@@ -1,10 +1,12 @@
 import argparse
 import os
+import tempfile
+import math
 
 
 def merge_sort(a):  # функция сортировки
     if len(a) > 1:
-        half = int((len(a) + 1) / 2)
+        half = int((len(a)+1) / 2)
         left_a = a[:half]
         right_a = a[half:]
         merge_sort(left_a)
@@ -12,10 +14,10 @@ def merge_sort(a):  # функция сортировки
         i = j = k = 0
         while i < len(left_a) and j < len(right_a):
             if left_a[i] < right_a[j]:
-                a[k] = left_a[i]
+                a[k]= left_a[i]
                 i += 1
             else:
-                a[k] = right_a[j]
+                a[k]= right_a[j]
                 j += 1
             k += 1
         while i < len(left_a):
@@ -27,13 +29,36 @@ def merge_sort(a):  # функция сортировки
             j += 1
             k += 1
 
+MEM_LIMIT = 400000000  # ограничение в памяти
 
-def sort_line(fi):  # сортировка строк
-    for line in fi:
+
+def merge_sort_file(file):
+    f = open(file)
+    temp_files = []
+    lines = []
+    bytes = 0
+    while True:
+        line = f.readline()
+        bytes += len(line)
+        if line == '':
+            break
         line = line.split()
         merge_sort(line)
-        line = ' '.join(line)
-        fi.write(line)
+        lines.append(' '.join(line) + '\n')
+        if bytes > MEM_LIMIT:
+            merge_sort(lines)
+            temp_files.append(write_temp_file(lines))
+            lines = []
+            bytes = 0
+    merge_sort(lines)
+    temp_files.append(write_temp_file(lines))
+
+
+def write_temp_file(lines):  # создаем временные файлы 
+    tmp = tempfile.TemporaryFile()
+    for line in lines:
+        tmp.append(line.encode('utf-8'))
+    return tmp
 
 
 def main():
@@ -44,7 +69,8 @@ def main():
         txtfile = str(arg.title) + '.txt'
     else:
         txtfile = input("введите имя файла:\n") + '.txt'
-    file = open(txtfile, 'w')
+    merge_sort_file(txtfile)
+
 
 
 
